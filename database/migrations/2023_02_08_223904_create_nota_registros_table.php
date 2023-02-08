016c32a3110d53714b41ee90be73b3118cee2079
Hernan820 <93552370+Hernan820@users.noreply.github.com>
2023-02-08 16:49:51 -0600

    recursos de la tbl notas de registro

---
+++ database/migrations/2023_02_08_223904_create_nota_registros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nota_registros', function (Blueprint $table) {
+            $table->id();
+            $table->text('nota_registro')->nullable(); 
+            $table->unsignedBigInteger('id_registro');
+            $table->foreign('id_registro')->references('id')->on('registros');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nota_registros');
+    }
+};
