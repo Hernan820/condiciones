c2386b36d1c024e81f1225ecf635476253902051
Bryan-Al <bryanperlera@gmail.com>
2023-03-07 11:21:53 -0600

    Recursos de la tabla tareas

---
+++ app/Http/Controllers/TareasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tareas;
+use Illuminate\Http\Request;
+
+class TareasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tareas  $tareas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tareas $tareas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tareas  $tareas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tareas $tareas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tareas  $tareas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tareas $tareas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tareas  $tareas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tareas $tareas)
+    {
+        //
+    }
+}
